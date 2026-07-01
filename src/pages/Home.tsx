3ece14866e96df8232e42d5795d66dc94ed7e9d8
kinjal-webdev <kinjal3969@gmail.com>
2026-07-01 11:29:56 +0530

    revert: remove curved marquee component and Home.tsx import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useLayoutEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import CurvedMarquee from "@/components/CurvedMarquee";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -25,7 +24,6 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
-        <CurvedMarquee />
         <About />
         <Services />
         <WhyChooseUs />
